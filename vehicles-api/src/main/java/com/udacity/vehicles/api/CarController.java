655ed4f6b5f791d79b1231d39d7107281b89ff7b
Babatunde <soft-sky@live.co.uk>
2019-12-07 14:09:13 +0100

    Implement a feature that delete a car

--- vehicles-api/src/main/java/com/udacity/vehicles/api/CarController.java
+++ vehicles-api/src/main/java/com/udacity/vehicles/api/CarController.java
@@ -83,8 +83,7 @@ class CarController {
          * TODO: Use the `assembler` on that updated car and return as part of the response.
          *   Update the first line as part of the above implementing.
          */
-        car.setId(id);
-        Car resource = carService.save(car);
+        Car resource = new Car();
         return ResponseEntity.ok(resource);
     }
 
