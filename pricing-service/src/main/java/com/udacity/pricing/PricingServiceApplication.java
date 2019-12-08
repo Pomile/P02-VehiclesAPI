fadac111228b50fb71d5142b234ff004fe8d9324
Babatunde <soft-sky@live.co.uk>
2019-12-08 14:29:39 +0100

    feat[pricing-microservice]: implement pricing microservice
    - [x] Setup eureka server
    - [x] Setup eureka client
    - [x] Populate pricing database

--- pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
+++ pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
@@ -1,8 +1,16 @@
 package com.udacity.pricing;
 
+import com.udacity.pricing.domain.price.Price;
+import com.udacity.pricing.domain.price.PriceRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Creates a Spring Boot Application to run the Pricing Service.
@@ -16,4 +24,18 @@ public class PricingServiceApplication {
         SpringApplication.run(PricingServiceApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner initDatabase(PriceRepository repository) {
+        return args -> {
+           for(int counter = 1; counter <= 20; counter++ ){
+
+               repository.save(new Price("USD", randomPrice(), (long) counter));
+           }
+        };
+    }
+
+    private BigDecimal randomPrice(){
+        return new BigDecimal(ThreadLocalRandom.current().nextDouble(1, 5))
+                .multiply(new BigDecimal(5000d)).setScale(2, RoundingMode.HALF_UP);
+    }
 }
